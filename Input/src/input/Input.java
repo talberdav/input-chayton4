1d9ee1da9cc9827ca7b4802be01e83b147880347
Ryan Bowlen <rabowlen@gmail.com>
2018-10-17 08:08:55 -0500

    Changed Print Statement

--- Input/src/input/Input.java
+++ Input/src/input/Input.java
@@ -33,7 +33,7 @@ public class Input {
           arr[3] = scan.nextLine();
           arr[4] = scan.nextLine();
           
-          out.printf("%s %s %s %s %s", arr[4], arr[3], arr[2], arr[1], arr[0]);
+          out.printf("%s %s %s %s %s", arr[0], arr[1], arr[2], arr[3], arr[4]);
           
           
           
